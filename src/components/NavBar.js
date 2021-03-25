f05a17c4ba661625b0b33ac752e315ac30194116
finerio <ariel.shadmon@gmail.com>
2021-03-25 11:56:42 +0100

    adding NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <NavLink to="/" activeStyle={{ fontWeight: "bold" }} exact>
+        Home
+      </NavLink>
+      {" - "}
+      <NavLink to="/about" activeStyle={{ fontWeight: "bold" }}>
+        About
+      </NavLink>
+      {" - "}
+      <NavLink to="/discover" activeStyle={{ fontWeight: "bold" }}>
+        Discover
+      </NavLink>
+    </div>
+  );
+}
